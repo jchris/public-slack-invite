74e10dbdb5c512acc32212c35778926f9c0894d9
Chris Anderson <chris@fauna.com>
2019-05-01 18:25:23 -0700

    request slack invite

--- functions/request-slack-invite/request-slack-invite.js
+++ functions/request-slack-invite/request-slack-invite.js
@@ -4,23 +4,16 @@ const axios = require("axios");
 exports.handler = async (event, context) => {
   try {
     const formData = parse(Buffer.from(event.body, 'base64').toString())
-    console.log("SLACK_LEGACY_TOKEN", process.env.SLACK_LEGACY_TOKEN)
-    console.log("formData", formData)
     const email = formData.email.trim();
     const baseURL = "https://slack.com/api/users.admin.invite"
-    const toSlack = `email=${encodeURIComponent(email)}&token=${SLACK_LEGACY_TOKEN}&set_active=true`;
-    const response = axios.get(`${baseURL}?${toSlack}`)
-    console.log(response);
-
-    // if (res.statusCode !== 200) {
-    //   console.error(res);
-    //   return { statusCode: 500, body: "error with slack api" }
-    // } else {
-    //
-    // }
-
-    const subject = event.queryStringParameters.name || "World"
-    return { statusCode: 200, body: JSON.stringify({ msg: `Hello ${subject}` }) }
+    const toSlack = `email=${encodeURIComponent(email)}&token=${process.env.SLACK_LEGACY_TOKEN}&set_active=true`;
+    const response = await axios.get(`${baseURL}?${toSlack}`)
+    console.log(response.data);
+    if (response.status !== 200) {
+      return { statusCode: 500, body: "error with slack api" }
+    } else {
+      return { statusCode: 200, body: JSON.stringify({ success: `Invited ${email}` }) }
+    }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
--- public/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  const message = document.getElementById("message")
-  document.getElementById("button").addEventListener("click", () => {
-    console.log("hello", message)
-    fetch("/.netlify/functions/hello-world?name=" + "Netlify")
-      .then((data) => data.json())
-      .then(({ msg }) => console.log(msg) || (message.innerHTML = msg))
-  })
-}

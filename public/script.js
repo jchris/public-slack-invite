db814288bc030278014e0e34fd2f6d9b989e9197
Summer Schrader <summer@Summers-MacBook-Pro.local>
2019-06-26 16:48:32 -0400

    UX-115 restyles slack signup page

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', (event) => {
   const match = location.hash.match(/invited=(.*)/)
   if (match && match.length > 1) {
     document.getElementById("flash-wrapper").innerHTML =
-    `<div id="flash">Invitation sent to ${match[1]}, check your email.</div>`
+    `<div id="flash">Invitation sent to ${match[1]}. Check your email.</div>`
     document.location.hash = ""
   }
 });
